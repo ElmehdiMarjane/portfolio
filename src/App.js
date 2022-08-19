19c49f540649a8e55677a8e6fe0a2198a75aa6e3
Elmehdi marjane <johndoe@example.com>
2022-08-19 02:14:50 +0100

    Responsive Navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Component }  from 'react';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
    
-<h1> Hello world!</h1>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-
+   <Navbar />
     </div>
   );
 }
